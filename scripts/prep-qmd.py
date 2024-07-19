7224ab3847634379785b3876b9c280b55eb9165b
Philip Chodrow <pchodrow@gmail.com>
2024-07-19 10:48:20 -0400

    restore live notebook line

--- scripts/prep-qmd.py
+++ scripts/prep-qmd.py
@@ -23,25 +23,25 @@ for f in os.listdir("source"):
             cleaned = file.read().replace('#---\n', '')
             
             live_path = f"{f.replace('.qmd', '.ipynb')}"
-            if live_path in os.listdir("docs/live-notebooks"):
+            # if live_path in os.listdir("docs/live-notebooks"):
                 
                 # link_line = f"*Download the live notebook corresponding to these notes [here](../live-notebooks/{live_path}).* \n"
                 
-                link_line = f"*Open the live notebook in Google Colab [here](https://colab.research.google.com/github/network-science-notes/network-science-notes.github.io/blob/main/docs/live-notebooks/{live_path}).* \n"
-                
-                cleaned_lines = cleaned.split("\n")
-                ix = 0
-                found = False
-                while not found: 
-                    line = cleaned_lines[ix]
-                    if line: 
-                        found = "#" in line 
-                    ix += 1
-                ix += 1    
-                
-                
-                cleaned_lines.insert(ix, link_line)            
-                cleaned = "\n".join(cleaned_lines)
+            link_line = f"*Open the live notebook in Google Colab [here](https://colab.research.google.com/github/network-science-notes/network-science-notes.github.io/blob/main/docs/live-notebooks/{live_path}).* \n"
+            
+            cleaned_lines = cleaned.split("\n")
+            ix = 0
+            found = False
+            while not found: 
+                line = cleaned_lines[ix]
+                if line: 
+                    found = "#" in line 
+                ix += 1
+            ix += 1    
+            
+            
+            cleaned_lines.insert(ix, link_line)            
+            cleaned = "\n".join(cleaned_lines)
             
             with open(f"chapters/{f}", "w") as new: 
                 new.write(cleaned)
