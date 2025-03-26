e032019f5a4455485a47ce3bebd5c8513fa692d2
Philip Chodrow <pchodrow@gmail.com>
2025-03-26 09:29:55 -0400

    Update

--- scripts/remove-hidden.py
+++ scripts/remove-hidden.py
@@ -1,7 +1,7 @@
 import shutil
 import os
 import re
-
+from bs4 import BeautifulSoup
 # https://stackoverflow.com/questions/12683834/how-to-copy-directory-recursively-in-python-and-overwrite-all
 
 def copy_and_overwrite(from_path, to_path):
@@ -11,18 +11,29 @@ def copy_and_overwrite(from_path, to_path):
 
 if not os.path.isdir("docs/full-notes"):
     os.makedirs("docs/full-notes", exist_ok=True)
-    
+
+# f = "10-configuration-model.html"
+
+# soup = BeautifulSoup(html,"html.parser")
+
+
+# for div in soup.find_all("div", {'class':'hide'}):
+#     div.decompose()
+
 for f in os.listdir("docs/chapters"):
     if f.endswith(".html"):
         with open(f"docs/chapters/{f}", "r") as file: 
             html = file.read()
         
-        regex = r'<div class=.*?hide.*?">[\S\s]*?</div>'
-        cleaned = re.sub(regex, "", html)
-        
         with open(f"docs/full-notes/{f}", "w") as new:
             new.write(html)
         
+        soup = BeautifulSoup(html, "html.parser")
+        
+        for div in soup.find_all("div", {'class':'hide'}):
+            div.decompose()
+        
+        cleaned = str(soup)
         with open(f"docs/chapters/{f}", "w") as file: 
             file.write(cleaned)
             
